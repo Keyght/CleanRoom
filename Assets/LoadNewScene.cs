0177a9220eb87b87ab74ea9e07d155f97a04652c
Marina <mbakanova2016@yandex.ru>
2020-10-21 20:09:20 +0300

    Added flat

---
+++ Assets/LoadNewScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Valve.VR;
+
+public class LoadNewScene : MonoBehaviour { 
+
+
+
+    public string sceneToLoad;
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+}
